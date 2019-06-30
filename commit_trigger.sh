1e6240294ece482395c36064eadd078408f360a8
publol <kto_v_teremo4ke@mail.ru>
2019-06-30 07:51:54 +0400

    Debug

--- commit_trigger.sh
+++ commit_trigger.sh
@@ -1,11 +1,11 @@
 if [ -f "commitFile.txt" ]; then
-    touch commitFile.txt
-    git add commitFile.txt
-    git commit -m "Commiting new file..."
-    git push origin develop
-else
     rm commitFile.txt
     git add -A
     git commit -m "Commiting delete of file..."
+    git push origin develop
+else
+    touch commitFile.txt
+    git add commitFile.txt
+    git commit -m "Commiting new file..."
     git push origin develop
 fi
