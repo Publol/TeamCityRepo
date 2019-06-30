659fd7ff2e98c79058132ca63018361fb85511b5
publol <kto_v_teremo4ke@mail.ru>
2019-06-30 05:07:42 +0400

    Fix

--- src/main/java/cucumber/steps/Background.java
+++ src/main/java/cucumber/steps/Background.java
@@ -4,10 +4,12 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.hooks.BaseHooks;
 import enums.Environment;
-import pages.AbstractPage;
+import org.openqa.selenium.NotFoundException;
 import pages.MainPage;
 import pages.MainPageLogIn;
 import pages.enums.EMainTopBar;
+
+import java.util.Objects;
 
 public class Background extends AbstractStep {
 
@@ -17,6 +19,9 @@ public class Background extends AbstractStep {
 
     @Given("^The \\\"([^\\\"]*)\\\" should be opened$")
     public void the_url_should_be_opened(String url) throws Throwable {
+        if (Objects.isNull(eDriver.getWebDriver())){
+            throw new NotFoundException("Driver is null");
+        }
         String requiredTitle = "STORE";
         MainPage mainPage = MainPage.launch(eDriver);
         assert eDriver.getWebDriver().getCurrentUrl().equals(url);
